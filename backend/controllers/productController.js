072d0e43f0e2c33482f17819198e5ff590be622d
Rohit Tamboli <tambolirohit2412@gmail.com>
2026-02-26 11:25:13 +0530

    first commit

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -35,7 +35,8 @@ export const paymentVerification = async (req, res) => {
   const isAuthentic = expectedSignature === razorpay_signature;
   if (isAuthentic) {
     return res.redirect(
-      `http://localhost:5173/paymentSuccess?reference=${razorpay_payment_id}`
+      // `http://localhost:5173/paymentSuccess?reference=${razorpay_payment_id}`
+      `https://update-page-nu.vercel.app/paymentSuccess?reference=${razorpay_payment_id}`
     );
   } else {
     res.status(400).json({
